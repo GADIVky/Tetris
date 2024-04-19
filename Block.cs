13a72c0a5b9df59ae322139e8ee0956233df2606
GADIVky <150321378+GADIVky@users.noreply.github.com>
2024-04-19 14:21:26 -0400

    Uploaded files
    I uploaded the files of the Tetris game, I finally completed will add comments very soon.

---
+++ Block.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTetris
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffset { get; } // Deceides where blocks spawn w/ the grid //
+        public abstract int Id { get; } // To distingish the blocks //
+        private int rotationState; //Stores current rotation state //
+        private Position offset; // Stores current offset state //
+
+        
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach (Position p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+
+        // Method rotates the Block 90 degrees Clockwise //
+        public void RotateCW()
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+            
+        // Rotates Counter Clockwise //
+        public void RotateCCW()
+        {
+            if (rotationState == 0)
+            {
+                rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationState--;
+            }
+
+        }
+
+        public void Move(int rows, int columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+
+        public void Reset()
+        {
+            rotationState = 0;
+            offset.Row = StartOffset.Row;
+            offset.Column = StartOffset.Column;
+        }
+    }
+}
